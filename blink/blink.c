d2b77f4230813a8fdad82ab8425c515164bd1b36
noda <noda@ac.cyberhome.ne.jp>
2023-07-22 05:21:21 +0900

    d

--- blink/blink.c
+++ blink/blink.c
@@ -13,6 +13,8 @@ int main() {
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+     
+     // test
     while (true) {
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
